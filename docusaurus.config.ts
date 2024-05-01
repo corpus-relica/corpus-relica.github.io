3feba8b70551523a5327bc7064a5f131811f7401
Marc Christophe <fusupo@gmail.com>
2024-05-01 12:48:25 -0700

    update base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://docs.relica.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/docs/",
 
   // GitHub pages deployment config.
   organizationName: "corpus-relica", // Usually your GitHub org/user name.

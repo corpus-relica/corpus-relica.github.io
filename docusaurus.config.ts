6e1dbb2ba36845d3b55514cea06d60c0892c770a
Marc Christophe <fusupo@gmail.com>
2024-05-02 12:14:54 -0700

    update logo.svg

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -130,7 +130,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Corpus Relica Generalis, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
